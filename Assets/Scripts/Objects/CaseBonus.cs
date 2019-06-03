cc043faf2e4f9df245ae215dba85ec3712b357ab
Forley <yanisguerault@gmail.com>
2019-06-03 21:02:28 +0200

    Add bonus spawn

---
+++ Assets/Scripts/Objects/CaseBonus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseBonus : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Bonus;
+    private bool m_activated = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!m_activated && collision.gameObject.GetComponent<PlayerController>())
+        {
+            m_activated = true;
+            Vector3 pos = transform.position + new Vector3(0,0.5f,-0.3f);
+            Quaternion rot = Quaternion.Euler(0, 0, 0);
+            Instantiate(m_Bonus, pos,rot);
+            
+        }
+    }
+}
